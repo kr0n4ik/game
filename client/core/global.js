e68749bba1bc6c1379757465ee3047cea847325c
kr0n4ik <kr0n4ik@hotmail.com>
2017-10-18 15:48:23 +0300

    r
    r

--- client/core/global.js
+++
@@ -1,9 +0,0 @@
-var socket = new WebSocket("ws://localhost:9998/");
-var scene = new THREE.Scene();
-var clock = new THREE.Clock();
-var width = window.innerWidth;
-var height = window.innerHeight;
-var camera = new THREE.PerspectiveCamera( 30, width / height, 0.1, 20000 );
-var models = [];
-var world = [];
-var me;
